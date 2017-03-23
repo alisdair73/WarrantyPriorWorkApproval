0cc9d85d17ed9a5f8a93c62e16fe7abbe7ae7d43
alisdair73 <alisdair73@gmail.com>
2017-03-24 09:14:32 +1100

    Initial Commit

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("hnd.dpe.warranty.prior_work_approval.controller.View1", {
+
+	});
+});
