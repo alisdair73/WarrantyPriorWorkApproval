332954b739b53a47104d819601a37f1262ff01cf
alisdair73 <alisdair73@gmail.com>
2017-03-29 21:16:31 +1100

    Attachments

--- webapp/block/SupportingDocumentsBlockController.controller.js
+++ webapp/block/SupportingDocumentsBlockController.controller.js
@@ -1,5 +1,6 @@
-sap.ui.define(["sap/ui/core/mvc/Controller" 
-], function(Controller) {
+sap.ui.define(["sap/ui/core/mvc/Controller",
+"sap/m/UploadCollectionParameter"
+], function(Controller, UploadCollectionParameter) {
 	"use strict";
 
 	return Controller.extend("hnd.dpe.warranty.prior_work_approval.block.SupportingDocumentsBlockController", {
@@ -11,6 +12,40 @@ sap.ui.define(["sap/ui/core/mvc/Controller"
 					this.getView().byId("attachmentTitle").setText(this._getAttachmentTitleText());
 				}.bind(this)
 			});
+		},
+
+		onUploadComplete: function(oEvent) {
+		},
+		
+		onUploadTerminated: function(oEvent) {
+		
+			var sFileName = oEvent.getParameter("fileName");
+			var oRequestHeaders = oEvent.getParameters().getHeaderParameter();
+		},
+		
+		onBeforeUploadStarts: function(oEvent){
+			
+			var oCustomerHeaderSlug = new sap.m.UploadCollectionParameter({
+				name : "slug",
+				value : oEvent.getParameter("fileName")
+			});
+			oEvent.getParameters().addHeaderParameter(oCustomerHeaderSlug);
+		},
+		
+		onChange: function(oEvent) {
+			var oUploadCollection = oEvent.getSource();
+			var oCustomerHeaderToken = new UploadCollectionParameter({
+				name : "x-csrf-token",
+				value : this.getView().getModel().getSecurityToken()
+			});
+			oUploadCollection.addHeaderParameter(oCustomerHeaderToken);
+		},
+ 
+		onFileDeleted: function(oEvent) {
+			this.deleteItemById(oEvent.getParameter("documentId"));
+		},		
+		
+		onFileSizeExceed : function(oEvent) {
 		},
 
 		_getAttachmentTitleText: function(){
