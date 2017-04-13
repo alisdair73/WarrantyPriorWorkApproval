b0be3cb96a8b6e99211ad220335560827c2ab4d7
alisdair73 <alisdair73@gmail.com>
2017-04-13 12:12:23 +1000

    Contributions

--- webapp/block/RepairCostsBlockController.controller.js
+++ webapp/block/RepairCostsBlockController.controller.js
@@ -1,11 +1,22 @@
 sap.ui.define(["sap/ui/core/mvc/Controller",
 "sap/m/MessageToast",
 "sap/ui/model/Filter",
-"sap/m/UploadCollectionParameter"
-], function(Controller, MessageToast, Filter, UploadCollectionParameter) {
+"sap/m/UploadCollectionParameter",
+"sap/ui/model/json/JSONModel"
+], function(Controller, MessageToast, Filter, UploadCollectionParameter, JSONModel) {
 	"use strict";
 
 	return Controller.extend("hnd.dpe.warranty.prior_work_approval.block.RepairCostsBlockController", {
+		
+		onInit: function() {
+    		
+			var oViewModel = new JSONModel({
+				"PartsSplitHonda": 0,
+				"LabourSplitHonda": 0,
+				"SubletSplitHonda": 0
+			});
+			this.getView().setModel(oViewModel, "CostsHelper");
+    	},
 		
 		calculateTotalCost: function(){
 			
